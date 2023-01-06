daf2687e492b7428d0a40951f73cdffad0b4a648
50bvd <loup.rks@bk.ru>
2023-01-06 09:40:16 +0100

    Update change-mac.pl

--- change-mac.pl
+++ change-mac.pl
@@ -1,20 +1,15 @@
 #!/usr/bin/perl
 
-# Find network interface
 $interface = $ARGV[0];
 
-# Check interface
 if (!defined($interface)) {
     print "Usage: change-mac.pl <interface>\n";
     exit;
 }
 
-# Generate new mac address
 $new_mac = join(':', map { sprintf "%02x", rand(256) } (1..6));
 
-# Inject new mac address
 system("ifconfig $interface down hw ether $new_mac");
 system("ifconfig $interface up");
 
-# Display new mac address
 print "Adresse MAC de l'interface $interface changée en $new_mac\n";
