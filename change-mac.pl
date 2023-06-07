85e22c9f9a978af1d24290d1fa64fe62f79a6037
loup.rks@bk.ru <loup.rks@bk.ru>
2023-06-08 00:21:33 +0200

    Update change-mac.pl

--- change-mac.pl
+++ change-mac.pl
@@ -1,15 +1,14 @@
 #!/usr/bin/perl
-
+#var
 $interface = $ARGV[0];
-
+$new_mac = join(':', map { sprintf "%02x", rand(256) } (1..6));
+#check if the interface variable is defined print usage message and exit if not
 if (!defined($interface)) {
     print "Usage: change-mac.pl <interface>\n";
     exit;
 }
-
-$new_mac = join(':', map { sprintf "%02x", rand(256) } (1..6));
-
+#desactivate interface sets new mac address and activates interface
 system("ifconfig $interface down hw ether $new_mac");
 system("ifconfig $interface up");
-
+#print success msg
 print "MAC address of the interface $interface changed to $new_mac\n";
